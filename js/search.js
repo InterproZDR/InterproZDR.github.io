bc946226f1e300ecca02c2dffb668fa770ec527c
snangofficial <2910305528@qq.com>
2020-04-18 16:48:43 +0800

    Revert "Revert "改一点细节""
    This reverts commit f94103005cf1ae155b1c1b81bac56a4fbdf6ce41.

--- js/search.js
+++ js/search.js
@@ -22,6 +22,7 @@ var engineList = [null,
   new Engine('百度', 'https://www.baidu.com/s?wd=%s', 'https://www.baidu.com/'),
   new Engine('必应', 'https://cn.bing.com/search?q=%s', 'https://cn.bing.com/'),
   new Engine('知乎', 'https://www.zhihu.com/search?type=content&q=%s', 'https://www.zhihu.com/'),
+  new Engine('A站', 'https://www.acfun.cn/search?keyword=%s', 'https://www.acfun.cn/'),
   new Engine('B站', 'https://search.bilibili.com/all?keyword=%s&from_source=banner_search', 'https://www.bilibili.com'),
   new Engine('萌百', 'https://zh.moegirl.org/index.php?search=%s', 'https://zh.moegirl.org/Mainpage'),
   new Engine('翻译', 'https://translate.google.cn/#view=home&op=translate&sl=auto&tl=zh-CN&text=%s', 'https://translate.google.cn/')];
@@ -49,6 +50,8 @@ function changeEngine(eChangeDirection) {
   document.getElementById('e-left').innerHTML = engineList[le].name;
   document.getElementById('e-middle').innerHTML = engineList[mid].name;
   document.getElementById('e-right').innerHTML = engineList[ri].name;
+  document.getElementById('schbox').focus();
+  schbox_onclick();
 }
 
 // 主要处理schbox按下回车键
@@ -62,7 +65,7 @@ function schbox_onkeydown() {
 }
 function schbox_onkeypress() {
   if (document.getElementById('schbox').value != "")
-    document.getElementById('clean-line').style.width = "488px";
+    document.getElementById('clean-line').style.width = "570px";
   else
     document.getElementById('clean-line').style.width = "0px";
 }
@@ -143,4 +146,3 @@ function warma() {
 
 // 沃尔玛（误）
 /* 我已经完全爱上Warma啦！*/
-  
